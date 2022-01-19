87f1e9739628f8e50bc3019cc11bead8833ea1d1
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2022-01-19 22:35:42 +0100

    creates Alert component

---
+++ src/components/Content/Alert/Alert.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Alert = () => {
+  return (
+    <div>
+      <div className="alert alert-success alert-dismissible" role="alert">
+        <h4 className="alert-heading">
+          Good Job! You have pressed the button!
+        </h4>
+        {/* to create a close button */}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+
+        <hr />
+        <p className="mb-0">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Alert;
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Alert from "./Alert/Alert";
 
 const Content = () => {
-  return <div>Content</div>;
+  const [activeButton, setActiveButton] = useState(false);
+
+  return (
+    <div>
+      {!activeButton && (
+        <button
+          type="button"
+          className="btn btn-danger btn-lg"
+          onClick={() => setActiveButton(true)}
+        >
+          Alert button
+        </button>
+      )}
+
+      {activeButton && <Alert />}
+    </div>
+  );
 };
 
 export default Content;
