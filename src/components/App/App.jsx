1cc114653bafa8b271424688282d780f8a1d56ed
solarenergy990 <74872776+solarenergy990@users.noreply.github.com>
2022-01-19 09:37:51 +0100

    Cretes base components: footer, header, nav

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Header from "../Header/Header";
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <main>
+        <Header />
+        <Nav />
+        <Footer />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <nav>Nav</nav>
+    </div>
+  );
+};
+
+export default Nav;
